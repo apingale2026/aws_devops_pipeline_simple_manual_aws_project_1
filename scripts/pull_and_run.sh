253238e22ed5835cb9314a1edf3438eb91c82f0d
apingale2026 <apingale2026@gmail.com>
2026-04-01 21:26:00 +0530

    Create pull_and_run.sh

---
+++ scripts/pull_and_run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Pull latest image from ECR
+aws ecr get-login-password --region us-east-1 \
+  | docker login --username AWS --password-stdin 533267311092.dkr.ecr.us-east-1.amazonaws.com
+
+docker pull 533267311092.dkr.ecr.us-east-1.amazonaws.com/$ImageName:latest
+
+# Run container
+docker run -d --name myapp -p 80:80 533267311092.dkr.ecr.us-east-1.amazonaws.com/$ImageName:latest
